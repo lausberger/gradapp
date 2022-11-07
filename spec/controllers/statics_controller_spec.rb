7b5241cee5b18d9ca16540b32c8b3ac038bd8a11
abeschen <abeschen@uiowa.edu>
2022-11-07 16:04:05 -0600

    Had to add monkeypatch to spec file for Thread Error: Already Initialized

--- spec/controllers/statics_controller_spec.rb
+++ spec/controllers/statics_controller_spec.rb
@@ -4,8 +4,21 @@ require 'rails_helper'
 describe StaticsController do
   describe 'Home Page' do
     it 'should render the statics template for home page' do
-      get "/home"
+      get "home"
       expect(response).to render_template('home')
+    end
+  end
+  if RUBY_VERSION>='2.6.0'
+    if Rails.version < '5'
+      class ActionController::TestResponse < ActionDispatch::TestResponse
+        def recycle!
+          @mon_mutex_owner_object_id = nil
+          @mon_mutex = nil
+          initialize
+        end
+      end
+    else
+      puts "Monkeypatch for ActionController::TestResponse no longer needed"
     end
   end
 end
