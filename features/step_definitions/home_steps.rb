93e8edf3f9d13dd16c55624a7b3dff1c5a69a633
abeschen <abeschen@uiowa.edu>
2022-11-07 16:15:30 -0600

    Fixed homepage cucumber test step

--- features/step_definitions/home_steps.rb
+++ features/step_definitions/home_steps.rb
@@ -1,11 +1,15 @@
-When /^ I have visited the GradApp Home Page$/ do
-  visit gradapp_path
+When('I have visited the GradApp Home Page') do
+  visit '/home'
 end
 
-When /^ I have clicked the link "(.*?)"$/ do |string|
+When('I have clicked {string}') do |string|
   click_on string
 end
 
-Then /^ I should see "(.*?)"$/ do |string|
+Then('I should see {string}') do |string|
   expect(page).to have_content(string)
+end
+
+Then('I should not see {string}') do |string|
+  expect(page).not_to have_content(string)
 end
