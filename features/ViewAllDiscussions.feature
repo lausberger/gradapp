bcb820e8d8f99d23924ab43e1c2b6536473779a6
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-07 08:59:00 -0600

    Updated ViewAllDiscussions features and added AddDiscussion features
    Signed-off-by: Jack Stockley <34308937+jnstockley@users.noreply.github.com>

--- features/ViewAllDiscussions.feature
+++ features/ViewAllDiscussions.feature
@@ -2,7 +2,7 @@ Feature: View the "Discussions" page to see all Discussions
 
   Scenario: View all the discussions on the main "discussions" page
 
-    Given I have added a discussion with the title "How do I sign up" and body "I am having issues signing up, how would I do that and" author "Jack Stockley"
+    Given I have added a discussion with the title "How do I sign up" and body "I am having issues signing up, how would I do that and" and author "Jack Stockley"
 
     When I have visited the main "Discussions" page
     Then I should see the discussion post by "Jack Stockley"
--- features/step_definitions/discussion_steps.rb
+++ features/step_definitions/discussion_steps.rb
@@ -1,4 +1,4 @@
-Given(/^I have added a discussion with the title "([^"]*)" and body "([^"]*)" author "([^"]*)"$/) do |arg1, arg2, arg3|
+Given(/^I have added a discussion with the title "([^"]*)" and body "([^"]*)" and author "([^"]*)"$/) do |arg1, arg2, arg3|
   pending
 end
 
@@ -7,5 +7,21 @@ When(/^I have visited the main "([^"]*)" page$/) do |arg|
 end
 
 Then(/^I should see the discussion post by "([^"]*)"$/) do |arg|
+  pending
+end
+
+And(/^I am on the discussions home page$/) do
+  pending
+end
+
+And(/^I am on the discussion page with the title "([^"]*)" and authored by "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+When(/^I post a reply with body "([^"]*)" and authored by "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+Then(/^I should see a reply with body "([^"]*)" and authored by "([^"]*)"$/) do |arg1, arg2|
   pending
 end
