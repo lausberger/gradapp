cdb2cb061c3a3e2d8435475c13bb525905f4109d
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-07 12:45:43 -0600

    Added DeleteDiscussion and EditDiscussion feature tests
    Signed-off-by: Jack Stockley <34308937+jnstockley@users.noreply.github.com>

--- features/step_definitions/discussion_steps.rb
+++ features/step_definitions/discussion_steps.rb
@@ -25,3 +25,35 @@ end
 Then(/^I should see a reply with body "([^"]*)" and authored by "([^"]*)"$/) do |arg1, arg2|
   pending
 end
+
+When(/^I have deleted the discussion with the title "([^"]*)" authored by "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+Then(/^I should not see the discussion post by "([^"]*)"$/) do |arg|
+  pending
+end
+
+And(/^I should be redirected to the discussion homepage$/) do
+  pending
+end
+
+And(/^There is a reply with body "([^"]*)" authored by "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+And(/^I should be redirected to the discussion page for the discussion with title "([^"]*)" authored by "([^"]*)"$/) do |arg1, arg2|
+  pending
+end
+
+And(/^I edit the discussion titled "([^"]*)" by "([^"]*)" with title "([^"]*)" and body "([^"]*)"$/) do |arg1, arg2, arg3, arg4|
+  pending
+end
+
+Then(/^I should see the discussion post by "([^"]*)" with title "([^"]*)" and body "([^"]*)"$/) do |arg1, arg2, arg3|
+  pending
+end
+
+When(/^I edit discussion reply with body "([^"]*)" authored by "([^"]*)" to body "([^"]*)"$/) do |arg1, arg2, arg3|
+  pending
+end
