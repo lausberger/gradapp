890d2c555469245aa168df9bb42ff9654bbab720
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-07 17:35:51 -0600

    Implemented on discussion page test, added variable names for remaining tests
    Signed-off-by: Jack Stockley <34308937+jnstockley@users.noreply.github.com>

--- features/step_definitions/discussion_steps.rb
+++ features/step_definitions/discussion_steps.rb
@@ -1,13 +1,25 @@
-Given(/^I have added a discussion with the title "([^"]*)" and body "([^"]*)" and author "([^"]*)"$/) do |arg1, arg2, arg3|
+Given(/^I have added a discussion with the title "([^"]*)" and body "([^"]*)" and author "([^"]*)"$/) do |title, body, author|
   pending
+  # TODO Create root discussion post
 end
 
-Given(/^There is a reply with body "([^"]*)" authored by "([^"]*)"$/) do |arg1, arg2|
+Given(/^There is a reply with body "([^"]*)" authored by "([^"]*)"$/) do |body, author|
   pending
+  # TODO Create root discussion post with reply
 end
 
-Given(/^I am on the discussion page with the title "([^"]*)" and authored by "([^"]*)"$/) do |arg1, arg2|
-  pending
+Given(/^I am on the discussion page with the title "([^"]*)" and authored by "([^"]*)"$/) do |title, author|
+  expected_page_title = "#{title} - #{author}"
+  all("tr").each do |tr|
+    post_title = tr.all("td")[0].text
+    post_author = tr.all("td")[2].text
+    if post_title.eql? title and post_author.eql? author
+      tr.all("td")[3].click_link
+      break
+    end
+  end
+  page_title = page.title
+  expect(page_title).eql? expected_page_title
 end
 
 When(/^I am on the discussions home page$/) do
@@ -18,19 +30,19 @@ When(/^I post a reply with body "([^"]*)" and authored by "([^"]*)"$/) do |body,
   pending
 end
 
-When(/^I have deleted the discussion with the title "([^"]*)" authored by "([^"]*)"$/) do |arg1, arg2|
+When(/^I have deleted the discussion with the title "([^"]*)" authored by "([^"]*)"$/) do |title, author|
   pending
 end
 
-When(/^I edit the discussion titled "([^"]*)" by "([^"]*)" with title "([^"]*)" and body "([^"]*)"$/) do |arg1, arg2, arg3, arg4|
+When(/^I edit the discussion titled "([^"]*)" by "([^"]*)" with title "([^"]*)" and body "([^"]*)"$/) do |old_title, author, new_title, new_body|
   pending
 end
 
-When(/^I edit discussion reply with body "([^"]*)" authored by "([^"]*)" to body "([^"]*)"$/) do |arg1, arg2, arg3|
+When(/^I edit discussion reply with body "([^"]*)" authored by "([^"]*)" to body "([^"]*)"$/) do |old_body, author, new_body|
   pending
 end
 
-Then(/^I should see the discussion post by "([^"]*)"$/) do |author|\
+Then(/^I should see the discussion post by "([^"]*)"$/) do |author|
   expect(find_post_by_author(author)).to be_truthy
 end
 
