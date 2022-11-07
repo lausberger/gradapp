ae5c10639faeef79a8a82e6ead2990edc3d86de8
abeschen <abeschen@uiowa.edu>
2022-11-07 15:31:16 -0600

    statics controller and routes created.

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,11 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  #resource :static
+  root :to => redirect('/statics')
+  get 'home' => 'statics'
+
+
   # Example resource route with options:
   #   resources :products do
   #     member do
