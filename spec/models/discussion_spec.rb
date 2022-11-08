cfc7112722de0764460dd1fb78dd32114d577df5
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-08 10:40:35 -0600

    Added describe statements for discussion tests
    Signed-off-by: Jack Stockley <34308937+jnstockley@users.noreply.github.com>

---
+++ spec/models/discussion_spec.rb
@@ -0,0 +1,34 @@
+describe Discussion do
+  describe "Add new discussion" do
+    context "root discussion" do
+
+    end
+    context "reply to discussion" do
+
+    end
+  end
+  describe "Edit existing discussion" do
+    context "root discussion" do
+
+    end
+    context "reply to discussion" do
+
+    end
+  end
+  describe "delete discussion" do
+    context "root discussion" do
+
+    end
+    context "reply to discussion" do
+
+    end
+  end
+  describe "Get discussions" do
+    context "all root discussions" do
+
+    end
+    context "all replys to discussion" do
+
+    end
+  end
+end
