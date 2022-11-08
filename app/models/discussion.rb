8f9f6ed099daed9a1665fbe085cf6604216a1e5e
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-08 13:41:30 -0600

    Implemented Discussion Model
    Signed-off-by: Jack Stockley <34308937+jnstockley@users.noreply.github.com>

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,14 +1,14 @@
 class Discussion < ActiveRecord::Base
 
   def get_root_posts
-
+    Discussion.find_by(root_discussion_id: 0)
   end
 
   def get_root_post(root_discussion_id)
-
+    Discussion.find_by(id: root_discussion_id)
   end
 
-  def get_post_replies(root_discussion_id)
-
+  def get_post_replies(post_root_discussion_id)
+    Discussion.find_by(root_discussion_id: post_root_discussion_id)
   end
 end
