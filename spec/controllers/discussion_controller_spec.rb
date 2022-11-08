0c0d227acc4f068227865a9ee4eeb42e802eb55a
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-08 12:50:11 -0600

    Added controller tests
    Signed-off-by: Jack Stockley <34308937+jnstockley@users.noreply.github.com>

---
+++ spec/controllers/discussion_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 'rails_helper'
+
+describe DiscussionsController do
+  describe 'reading discussion posts' do
+    it 'should load the index template with discussions' do
+
+    end
+    it 'should load the show template and show a discussion with its replies' do
+
+    end
+  end
+  describe 'adding discussion post' do
+    it 'should add selected root discussion to db and load index template' do
+
+    end
+    it 'should add reply to selected discussion to db and load show template for root discussion' do
+
+    end
+  end
+  describe 'editing discussion post' do
+    it 'should edit root discussion in db and load index template' do
+
+    end
+    it 'should edit reply to selected discussion in db and load show template for root discussion' do
+
+    end
+  end
+  describe 'delete discussion post' do
+    it 'should delete root discussion from db and load index template' do
+
+    end
+    it 'should delete reply to selected discussion from db and load show template for root discussion' do
+
+    end
+  end
+end
