b54ba646c8806e94228e18efc41bb213c3ec505a
Lucas Ausberger <ausbergerl@hotmail.com>
2022-11-08 17:26:09 -0600

    basic accounts controller methods

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,22 @@
 class AccountsController < ApplicationController
+    def new
+        render :new
+    end
+
+    def create
+        field_data = registration_params
+        account_params = field_data.slice(:first, :last, :email, :password, :type)
+        @account = Account.new(account_params)
+        if @account.save
+            # redirect_to root_path, notice: 'Account registration successful'
+        else
+            flash[:alert] = "Failed to create user account. Please try again."
+            render :new
+        end
+    end
+
+    private
+    def registration_params
+        params.require(:account).permit(:first, :last, :email, :password, :password_confirm, :type)
+    end
 end
