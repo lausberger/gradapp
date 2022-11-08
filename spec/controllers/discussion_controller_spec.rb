13ba26abf7bff4528a839f96d2252ceb1196d5b7
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-08 13:28:50 -0600

    Implemented spec tests
    Signed-off-by: Jack Stockley <34308937+jnstockley@users.noreply.github.com>

--- spec/controllers/discussion_controller_spec.rb
+++ spec/controllers/discussion_controller_spec.rb
@@ -2,36 +2,24 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe DiscussionsController do
-  describe 'reading discussion posts' do
-    it 'should load the index template with discussions' do
-
-    end
-    it 'should load the show template and show a discussion with its replies' do
-
-    end
-  end
   describe 'adding discussion post' do
     it 'should add selected root discussion to db and load index template' do
-
+      post :post_details, {:title => 'Title', :body => 'Body', :author => 'Author', :root_discussion_id => 0}
+      expect(response).to render_template('index')
     end
     it 'should add reply to selected discussion to db and load show template for root discussion' do
-
-    end
-  end
-  describe 'editing discussion post' do
-    it 'should edit root discussion in db and load index template' do
-
-    end
-    it 'should edit reply to selected discussion in db and load show template for root discussion' do
-
+      post :post_details, {:title => 'Title', :body => 'Body', :author => 'Author', :root_discussion_id => 1}
+      expect(response).to render_template('show')
     end
   end
   describe 'delete discussion post' do
     it 'should delete root discussion from db and load index template' do
-
+      delete :id, 1
+      expect(response).to render_template('index')
     end
     it 'should delete reply to selected discussion from db and load show template for root discussion' do
-
+      delete :id, 2
+      expect(response).to render_template('show')
     end
   end
 end
