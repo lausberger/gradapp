7232e41c930312e1215b8fdccac1a33f6a158989
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-08 13:01:16 -0600

    Implemented add discussion model tests
    Signed-off-by: Jack Stockley <34308937+jnstockley@users.noreply.github.com>

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -1,3 +1,14 @@
 class Discussion < ActiveRecord::Base
 
+  def get_root_posts
+
+  end
+
+  def get_root_post(root_discussion_id)
+
+  end
+
+  def get_post_replies(root_discussion_id)
+
+  end
 end
--- spec/models/discussion_spec.rb
+++ spec/models/discussion_spec.rb
@@ -1,10 +1,23 @@
 describe Discussion do
   describe "Add new discussion" do
     context "root discussion" do
-
+      post_title = "Root Discussion"
+      post_body = "Root discussion body"
+      post_author = "Admin"
+      discussion = Discussion.create!(title: post_title, body: post_body, author: post_author, root_discussion_id: 0)
+      expect(Discussion.get_root_posts).to include? discussion
+      expect(Discussion.get_root_post(discussion.id)).to eql? discussion
     end
     context "reply to discussion" do
-
+      root_post_title = "Root Discussion"
+      root_post_body = "Root discussion body"
+      root_post_author = "Admin"
+      root_discussion = Discussion.create!(title: root_post_title, body: root_post_body, author: root_post_author, root_discussion_id: 0)
+      reply_post_title = "Root Discussion"
+      reply_post_body = "Root discussion body"
+      reply_post_author = "Admin"
+      reply_discussion = Discussion.create!(title: reply_post_title, body: reply_post_body, author: reply_post_author, root_discussion_id: root_discussion.id)
+      expect(Discussion.get_post_replies(root_discussion.id)).to include? reply_discussion
     end
   end
   describe "Edit existing discussion" do
