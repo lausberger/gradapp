57217c1fe39a0233466e4f7705805915971055cd
Lucas Ausberger <ausbergerl@hotmail.com>
2022-11-09 16:08:05 -0600

    commented out routes for unimplemented features

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   post 'register', to: 'accounts#create'
   # consider adding separate 'sessions' controller for managing login sessions
   # https://www.section.io/engineering-education/how-to-setup-user-authentication-from-scratch-with-rails-6/
-  get 'login', to: 'accounts#login'
-  post 'login', to: 'accounts#authenticate'
-  delete 'logout', to: 'accounts#logout'
+  # get 'login', to: 'accounts#login'
+  # post 'login', to: 'accounts#authenticate'
+  # delete 'logout', to: 'accounts#logout'
 end
