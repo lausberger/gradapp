6c5bafa7b757522167cdc0026f90019bbb0092e6
Lucas Ausberger <ausbergerl@hotmail.com>
2022-11-09 16:07:26 -0600

    changed name fields back to snake case

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@
 ActiveRecord::Schema.define(version: 20221108224911) do
 
   create_table "accounts", force: :cascade do |t|
-    t.string   "first",           null: false
-    t.string   "last",            null: false
+    t.string   "first_name",      null: false
+    t.string   "last_name",       null: false
     t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.string   "type",            null: false
