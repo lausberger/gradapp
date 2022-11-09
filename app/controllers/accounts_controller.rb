bf52e4a4302a55a199711830a483c256bbb3e0ec
Lucas Ausberger <ausbergerl@hotmail.com>
2022-11-09 16:12:26 -0600

    rewrote create method to perform field validity checks

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,26 +2,44 @@ class AccountsController < ApplicationController
     def new
     end
 
+    # add a show method and view
+    # def show
+    # end
+
     def create
         field_data = registration_params
-        if field_data[:password] == field_data[:password_confirm]
-            account_params = field_data.slice(:first, :last, :email, :password, :type)
-            @account = Account.new(account_params)
-            if @account.save
-                flash[:notice] = "Account registration successful"
-                redirect_to root_path
-            else
-                flash[:alert] = "Account registration failed, please try again."
-                render :new
-            end
+        if field_data.any? { |k,v| v.blank? }
+            flash[:warning] = "Fields cannot be empty"
         else
-            flash[:alert] = "Passwords do not match"
-            redirect :new
+            if field_data[:password] != field_data[:password_confirm]
+                flash[:warning] = "Passwords do not match"
+            else
+                if field_data[:email] !~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+                    flash[:warning] = "Please enter a valid email address"
+                else
+                    account_params = field_data.slice(:first_name, :last_name, :email, :password, :type)
+                    @account = Account.new(account_params)
+                    if @account.save
+                        flash[:notice] = "Account registration successful"
+                        # redirect_to root_path
+                        render :new 
+                        return
+                    else
+                        if Account.where(email: @account.email)
+                            flash[:warning] = "An account with that email already exists"
+                        else
+                            flash[:alert] = "Account registration failed, please try again."
+                        end
+                    end
+                end
+            end
         end
+        # if we reach this line, something has gone wrong
+        render :new
     end
 
     private
     def registration_params
-        params.require(:account).permit(:first, :last, :email, :password, :password_confirm, :type)
+        params.require(:account).permit(:first_name, :last_name, :email, :password, :password_confirm, :type)
     end
 end
