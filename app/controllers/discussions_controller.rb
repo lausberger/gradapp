cd9d43fb40d7fa14a5ab61c13778b3bd0cfd310c
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-08 18:30:09 -0600

    Implemented show and index controller methods
    Signed-off-by: Jack Stockley <34308937+jnstockley@users.noreply.github.com>

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,16 +1,12 @@
 class DiscussionsController < ApplicationController
 
   def show
-    # TODO
     discussion_id = params[:id]
-    puts discussion_id
-    @root_discussion = {:id => 0, :title => "Title 1", :body => "Body 1", :author => "Author 1", :root_discussion_id => 0}
-    @discussions_with_replies = [{:id => 0, :title => "Title 1", :body => "Body 1", :author => "Author 1", :root_discussion_id => 0}, {:id => 1,:title => "Title 2", :body => "Body 2", :author => "Author 2", :root_discussion_id => 0}, {:id => 3,:title => "Title 3", :body => "Body 3", :author => "Author 3", :root_discussion_id => 0}]
+    @root_discussion = Discussion.get_root_post(discussion_id)
+    @discussions_with_replies = Discussion.get_post_replies(@root_discussion[:id])
   end
 
   def index
-    # TODO
-    #@discussions = [{:id => 0, :title => "Title 1", :body => "Body 1", :author => "Author 1", :root_discussion_id => 0}, {:id => 1,:title => "Title 2", :body => "Body 2", :author => "Author 2", :root_discussion_id => 0}, {:id => 3,:title => "Title 3", :body => "Body 3", :author => "Author 3", :root_discussion_id => 0}]
     @discussions = Discussion.get_root_posts
   end
 
