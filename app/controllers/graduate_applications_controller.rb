f311ec796f9174b047294cbde54f46194d617d04
Brandon Egger <beggr@uiowa.edu>
2022-11-09 21:46:27 -0600

    Added functionality and validation to GraduateApplication model

--- app/controllers/graduate_applications_controller.rb
+++ app/controllers/graduate_applications_controller.rb
@@ -1,3 +1,6 @@
 class GraduateApplicationsController < ApplicationController
+  def graduate_application_params
+    params.require(:graduate_application).permit(:first_name, :last_name, :email, :phone, :dob, :gpa, :gpa_out_of)
+  end
 
 end
--- app/models/graduate_application.rb
+++ app/models/graduate_application.rb
@@ -1,10 +1,40 @@
 class GraduateApplication < ActiveRecord::Base
   def self.all_status
-    {
-      :in_progress => 0,
-      :submitted => 1,
-      :denied => 3,
-      :accepted => 4,
+    %w(in-progress submitted denied accepted)
+  end
+
+  has_one :account
+  validates_associated :account
+
+  serialize :gpa
+
+  attr_accessor :first_name, :last_name, :email, :phone, :dob, :status
+  attr_reader :gpa
+
+  validates :first_name, :last_name, presence: true, format: {with: /\A\S+\z/, message: "No white spaces in names"}
+  validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
+  validates :phone, presence: true, format: {with: /\A\d{9,11}\z/, message: "only digits"}
+  validates :dob, presence: true
+  validates :gpa, presence: true
+  validates :status, inclusion: { in: self.all_status }
+
+  def gpa=(val)
+    raise ArgumentError unless val.is_a? Hash
+    raise ArgumentError unless val.has_key? :numerator and val.has_key? :denominator
+    raise ArgumentError unless val[:numerator].is_a? Numeric and val[:denominator].is_a? Numeric
+
+    @gpa = {
+      :numerator => val[:numerator],
+      :denominator => val[:denominator],
     }
   end
+
+  def gpa_ratio
+    @gpa[:numerator]/@gpa[:denominator];
+  end
+
+  def full_name
+    @first_name + " " + @last_name
+  end
+
 end
