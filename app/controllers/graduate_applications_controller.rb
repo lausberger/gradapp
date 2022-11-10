4b4c398bc4f06862788ef9c048ce140f2fb79e21
Brandon Egger <beggr@uiowa.edu>
2022-11-09 22:06:13 -0600

    added base views for graduate appliations controller

--- app/controllers/graduate_applications_controller.rb
+++ app/controllers/graduate_applications_controller.rb
@@ -3,4 +3,17 @@ class GraduateApplicationsController < ApplicationController
     params.require(:graduate_application).permit(:first_name, :last_name, :email, :phone, :dob, :gpa, :gpa_out_of)
   end
 
+  def show
+    id = params[:id]
+    @graduate_application = GraduateApplication.find(id)
+  end
+
+  def index
+    @graduate_applications = GraduateApplication.all
+  end
+
+  def new
+    # Navigates to new view
+  end
+
 end
