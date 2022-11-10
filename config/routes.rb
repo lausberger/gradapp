9fac992e5ef29a7e969210b786d9d336873f5549
Lucas Ausberger <ausbergerl@hotmail.com>
2022-11-09 18:52:26 -0600

    removed clutter, again

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,12 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
-  resources :discussions
-
-  post 'discussions/create_reply'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
->>>>>>> 1170142366f6b6d1c5d25ae1286badf532bc68a3
 
   resource :statics
   root :to => redirect('/home')
   get 'home' => 'statics'
   get 'faq' => 'statics'
+
+  resources :discussions
+  post 'discussions/create_reply'
 
   get 'register', to: 'accounts#new'
   post 'register', to: 'accounts#create'
