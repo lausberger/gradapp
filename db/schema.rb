f3da4c09608ea61ac26d1b7f43b362299692ecce
Lucas Ausberger <ausbergerl@hotmail.com>
2022-11-09 18:58:30 -0600

    schema conflict

--- db/schema.rb
+++ db/schema.rb
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
-
 ActiveRecord::Schema.define(version: 20221108224911) do
 
   create_table "accounts", force: :cascade do |t|
@@ -24,14 +10,11 @@ ActiveRecord::Schema.define(version: 20221108224911) do
     t.datetime "updated_at",      null: false
   end
 
-=======
-ActiveRecord::Schema.define do
-
   create_table "discussions" do |t|
     t.string "title"
     t.string "body"
     t.string "author"
     t.integer "root_discussion_id", default: -1
   end
->>>>>>> 1170142366f6b6d1c5d25ae1286badf532bc68a3
+
 end
