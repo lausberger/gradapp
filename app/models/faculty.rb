64937d7d0717fd9b52b7a8299994535d5af25d4f
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-15 18:03:02 -0600

    Added faculties db migration

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,13 +1,2 @@
-class Faculty < Account
-
-  validates :topic_area, presence: true
-
-  def self.get_all
-    Faculty.get_all
-  end
-
-  def self.get_by_topic_area(selected_topic_area)
-    Faculty.where(topic_area: selected_topic_area)
-  end
-
+class Faculty < ActiveRecord::Base
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 30721148119000) do
+ActiveRecord::Schema.define(version: 30721148119002) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "first_name",      null: false
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 30721148119000) do
     t.string   "root_discussion_id", default: "-1"
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+  end
+
+  create_table "faculties", force: :cascade do |t|
+    t.integer  "account_id"
+    t.string   "topic_are"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "graduate_applications", force: :cascade do |t|
