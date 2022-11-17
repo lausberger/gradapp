440bbd231b267965fbec0b63175679f1be16f9fe
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-17 11:43:42 -0600

    Updated faculty tests and added more model validation

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,4 +1,5 @@
 class Faculty < ActiveRecord::Base
   belongs_to :account
 
+  validates :topic_area, presence: true, allow_blank: false
 end
--- spec/models/faculty_spec.rb
+++ spec/models/faculty_spec.rb
@@ -17,41 +17,40 @@ if RUBY_VERSION>='2.6.0'
 end
 
 describe Faculty do
+  before(:each) do
+    account = {
+      :email => 'jnstockley@gmail.com',
+      :first_name => 'Jack',
+      :last_name => 'Stockley',
+      :password => 'Password1234',
+      :account_type => 'faculty',
+    }
+    @account_creation = Account.create!(account)
+    @faculty = {
+      :account_id => @account_creation.id,
+      :topic_area => 'CSE'
+    }
+  end
   describe 'topic area validation' do
-    before(:each) do
-      @faculty_account = {
-        :first_name => 'Jack',
-        :last_name => 'Stockley',
-        :email => 'jnstockley@uiowa.edu',
-        :password_digest => 'password123',
-        :type => 'Faculty',
-        :topic_area => 'CSE'
-      }
-    end
     it 'should fail on nil topic_area' do
-      @faculty_account[:topic_area] = nil
-      expect(Faculty.new(@faculty_account).valid?).to eq false
+      @faculty[:topic_area] = nil
+      expect(Faculty.new(@faculty).valid?).to eq false
     end
     it 'should pass on valid faculty account' do
-      @faculty_account[:topic_area] = 'CSE'
-      expect(Faculty.new(@faculty_account).valid?).to eq true
+      @faculty[:topic_area] = 'CSE'
+      expect(Faculty.new(@faculty).valid?).to eq true
     end
   end
   describe 'adding faculty account' do
     context 'Faculty' do
-      before(:all) do
-        @faculty_account = {
-          :first_name => 'Jack',
-          :last_name => 'Stockley',
-          :email => 'jnstockley@uiowa.edu',
-          :password_digest => 'password123',
-          :type => 'Faculty',
-          :topic_area => 'CSE'
-        }
-      end
       it 'should appear in CSE topic area' do
-        expect(Faculty.where(topic_are: @faculty_account[:topic_are])).to exist
+        @faculty[:topic_area] = 'CSE'
+        faculty_creation = Faculty.create!(@faculty)
+        expect(Faculty.where(topic_area: faculty_creation[:topic_area])).to exist
       end
+    end
+    after(:each) do
+      Account.destroy(@account_creation.id)
     end
   end
 end
