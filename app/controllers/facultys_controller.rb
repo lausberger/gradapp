023bdee5c535c29be1ad246236016e8ed7aaf5d5
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-19 10:18:28 -0600

    Ran rubocop on facultys_controller

--- app/controllers/facultys_controller.rb
+++ app/controllers/facultys_controller.rb
@@ -1,27 +1,26 @@
-class FacultysController < ApplicationController
+# frozen_string_literal: true
 
+# Controller class for Find Faculty
+class FacultysController < ApplicationController
   def index
     @faculties_and_topic_area = []
     faculties = Faculty.all
     faculties.each do |faculty|
       @faculties_and_topic_area << {
-        :first_name => faculty.account[:first_name],
-        :last_name => faculty.account[:last_name],
-        :topic_area => faculty[:topic_area]
+        first_name: faculty.account[:first_name],
+        last_name: faculty.account[:last_name],
+        topic_area: faculty[:topic_area]
       }
     end
   end
 
   def search
     @topic_area = params[:search_topic_area]
-    if @topic_area == '' or @topic_area == nil
-      flash[:warning] = "Invalid topic area specified!"
+    if (@topic_area == '') || @topic_area.nil?
+      flash[:warning] = 'Invalid topic area specified!'
       return redirect_to facultys_path
     end
-    @faculties = Faculty.where(:topic_area => @topic_area)
-    if @faculties.length == 0
-      flash[:warning] = "No Faculty members with the topic area specified!"
-      redirect_to facultys_path
-    end
+    @faculties = Faculty.where(topic_area: @topic_area)
+    redirect_to facultys_path if @faculties.length.zero?
   end
 end
