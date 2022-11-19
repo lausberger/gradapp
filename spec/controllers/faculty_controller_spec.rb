8f97dbf0b3d0fd683a2130bc27fc1435e4518d40
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-19 10:22:21 -0600

    Ran rubocop on faculty_controller_spec

--- spec/controllers/faculty_controller_spec.rb
+++ spec/controllers/faculty_controller_spec.rb
@@ -1,18 +1,22 @@
+# frozen_string_literal: true
+
 require 'spec_helper'
 require 'rails_helper'
 
-if RUBY_VERSION>='2.6.0'
+if RUBY_VERSION >= '2.6.0'
   if Rails.version < '5'
-    class ActionController::TestResponse < ActionDispatch::TestResponse
-      def recycle!
-        # hack to avoid MonitorMixin double-initialize error:
-        @mon_mutex_owner_object_id = nil
-        @mon_mutex = nil
-        initialize
+    module ActionController
+      class TestResponse < ActionDispatch::TestResponse
+        def recycle!
+          # HACK: to avoid MonitorMixin double-initialize error:
+          @mon_mutex_owner_object_id = nil
+          @mon_mutex = nil
+          initialize
+        end
       end
     end
   else
-    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+    puts 'Monkeypatch for ActionController::TestResponse no longer needed'
   end
 end
 
@@ -21,52 +25,52 @@ describe FacultysController do
     context 'search for professors in CSE topics are' do
       before(:all) do
         @account_one = {
-          :email => 'jnstockley@gmail.com',
-          :first_name => 'Jack',
-          :last_name => 'Stockley',
-          :password => 'Password1234',
-          :account_type => 'faculty',
+          email: 'jnstockley@gmail.com',
+          first_name: 'Jack',
+          last_name: 'Stockley',
+          password: 'Password1234',
+          account_type: 'faculty'
         }
         @account_two = {
-          :email => 'hans-johnson@uiowa.edu',
-          :first_name => 'Hans',
-          :last_name => 'Johnson',
-          :password => 'S3curEpA55w0rD',
-          :account_type => 'faculty',
+          email: 'hans-johnson@uiowa.edu',
+          first_name: 'Hans',
+          last_name: 'Johnson',
+          password: 'S3curEpA55w0rD',
+          account_type: 'faculty'
         }
         @account_three = {
-          :email => 'joe.smith@gmail.com',
-          :first_name => 'Joesph',
-          :last_name => 'Smith',
-          :password => 'iloveselt23',
-          :account_type => 'faculty',
+          email: 'joe.smith@gmail.com',
+          first_name: 'Joesph',
+          last_name: 'Smith',
+          password: 'iloveselt23',
+          account_type: 'faculty'
         }
         @account_four = {
-          :email => 'nikki-brown@yahoo.com',
-          :first_name => 'Nikki',
-          :last_name => 'Brown',
-          :password => 'rubyIS<3',
-          :account_type => 'faculty',
+          email: 'nikki-brown@yahoo.com',
+          first_name: 'Nikki',
+          last_name: 'Brown',
+          password: 'rubyIS<3',
+          account_type: 'faculty'
         }
         @account_one_creation = Account.create!(@account_one)
         @account_two_creation = Account.create!(@account_two)
         @account_three_creation = Account.create!(@account_three)
         @account_four_creation = Account.create!(@account_four)
         @faculty_one = {
-          :account_id => @account_one_creation.id,
-          :topic_area => 'Math'
+          account_id: @account_one_creation.id,
+          topic_area: 'Math'
         }
         @faculty_two = {
-          :account_id => @account_two_creation.id,
-          :topic_area => 'Rhetoric'
+          account_id: @account_two_creation.id,
+          topic_area: 'Rhetoric'
         }
         @faculty_three = {
-          :account_id => @account_three_creation.id,
-          :topic_area => 'CSE'
+          account_id: @account_three_creation.id,
+          topic_area: 'CSE'
         }
         @faculty_four = {
-          :account_id => @account_four_creation.id,
-          :topic_area => 'CSE'
+          account_id: @account_four_creation.id,
+          topic_area: 'CSE'
         }
         @faculty_one_creation = Faculty.create!(@faculty_one)
         @faculty_two_creation = Faculty.create!(@faculty_two)
@@ -78,16 +82,16 @@ describe FacultysController do
         expect(response).to render_template('index')
       end
       it 'should return to index when invalid topic area searched' do
-        post :search, {:search_topic_area => ''}
+        post :search, { search_topic_area: '' }
         expect(response).to redirect_to facultys_path
       end
       it 'should flash invalid topic area message' do
-        post :search, {:search_topic_area => 'fdfdfds'}
+        post :search, { search_topic_area: 'fdfdfds' }
         expect(response).to redirect_to facultys_path
         expect(flash[:warning]).to be_present
       end
       it 'should select facultys members only in CSE topic area' do
-        post :search, {:search_topic_area => 'CSE'}
+        post :search, { search_topic_area: 'CSE' }
         expect(response).to render_template('search')
       end
       after(:all) do
