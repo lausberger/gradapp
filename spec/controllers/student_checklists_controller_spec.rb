3e66664ec5b4940b25b26b4d850267aa12c43acb
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-21 13:37:23 -0600

    Implemented checklist controller spec tests

--- spec/controllers/student_checklists_controller_spec.rb
+++ spec/controllers/student_checklists_controller_spec.rb
@@ -17,5 +17,15 @@ if RUBY_VERSION>='2.6.0'
 end
 
 describe StudentChecklistsController do
-
+  describe 'completing new checklist item' do
+    before(:all) do
+      @student_checklist = {
+        :student_id => 1
+      }
+    end
+    it 'should redirect to checklist page' do
+      post :edit, {:checked_items => ['citizenship']}
+      expect(response).to redirect_to student_checklist_page
+    end
+  end
 end
