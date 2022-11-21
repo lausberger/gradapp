79700fd794d4372772aae4b2020d9ea49a7baf95
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-21 13:24:33 -0600

    Implemented checklist model spec tests

--- app/models/student_checklist.rb
+++ app/models/student_checklist.rb
@@ -1,3 +1,5 @@
 class StudentChecklist < ActiveRecord::Base
   belongs_to :student
+
+  validates :student_id, presence: true, uniqueness: true
 end
--- spec/models/student_checklist_spec.rb
+++ spec/models/student_checklist_spec.rb
@@ -16,5 +16,83 @@ if RUBY_VERSION>='2.6.0'
   end
 end
 describe StudentChecklist do
-
+  describe 'validation' do
+    before(:all) do
+      @student_checklist = {
+        :student_id => 1
+      }
+    end
+    it 'should pass since no checklists are present' do
+      expect(StudentChecklist.new(@student_checklist).valid?).to eq true
+    end
+    it 'should fail since checklist with student id is present' do
+      StudentChecklist.create!(@student_checklist)
+      expect(StudentChecklist.new(@student_checklist).valid?).to eq false
+    end
+  end
+  describe 'check default values' do
+    before(:all) do
+      @student_checklist = {
+        :student_id => 1
+      }
+      @student = StudentChecklist.create!(@student_checklist)
+    end
+    it 'check citizenship value' do
+      expect(@student[:citizenship]).to be false
+    end
+    it 'check research_area value' do
+      expect(@student[:citizenship]).to be false
+    end
+    it 'check degree_objective value' do
+      expect(@student[:research_area]).to be false
+    end
+    it 'check ug_inst value' do
+      expect(@student[:ug_inst]).to be false
+    end
+    it 'check ug_gpa value' do
+      expect(@student[:ug_gpa]).to be false
+    end
+    it 'check ug_degree value' do
+      expect(@student[:ug_degree]).to be false
+    end
+    it 'check ug_major value' do
+      expect(@student[:ug_major]).to be false
+    end
+    it 'check ug_transcript value' do
+      expect(@student[:ug_transcript]).to be false
+    end
+    it 'check grad_inst value' do
+      expect(@student[:grad_inst]).to be false
+    end
+    it 'check grad_gpa value' do
+      expect(@student[:grad_gpa]).to be false
+    end
+    it 'check grad_degree value' do
+      expect(@student[:grad_degree]).to be false
+    end
+    it 'check grad_major value' do
+      expect(@student[:grad_major]).to be false
+    end
+    it 'check grad_transcript value' do
+      expect(@student[:grad_transcript]).to be false
+    end
+    it 'check letter_recommendations value' do
+      expect(@student[:letter_recommendations]).to be false
+    end
+    it 'check gre_scores value' do
+      expect(@student[:gre_scores]).to be false
+    end
+    it 'check language_scores value' do
+      expect(@student[:language_scores]).to be false
+    end
+    it 'check resume value' do
+      expect(@student[:resume]).to be false
+    end
+    it 'check sop value' do
+      expect(@student[:sop]).to be false
+    end
+    after(:all) do
+      StudentChecklist.find(@student.id).destroy
+    end
+  end
 end
