06e322f58dd9557b011c922dc106f69905ad1830
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-21 14:27:00 -0600

    Implemented show method

--- app/controllers/student_checklists_controller.rb
+++ app/controllers/student_checklists_controller.rb
@@ -1,7 +1,9 @@
 class StudentChecklistsController < ApplicationController
 
   def show
-
+    student_id = params[:id]
+    @student = Account.find(student_id)
+    @student_checklist = StudentChecklist.find_by(:student_id => student_id)
   end
 
   def edit
