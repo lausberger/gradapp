42ba4e8891dd283c28d586f583973c4fdc260801
abeschen <abeschen@uiowa.edu>
2022-11-22 16:22:32 -0600

    Added message relationship to account model

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,6 +5,9 @@ class Account < ActiveRecord::Base
   # represents password in a secure manner, maps to password_digest
   has_secure_password
 
+  has_many :to_messages, class_name: 'Message', foreign_key: 'to_id'
+  has_many :from_messages, class_name: 'Message', foreign_key: 'from_id'
+
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i }
   validates :first_name, presence: true
