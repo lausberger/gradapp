a5b3a4739b9399bb622eea827d70bbabbab8ab22
abeschen <abeschen@uiowa.edu>
2022-11-22 16:24:28 -0600

    Added index, new, sendmessage functions to messagescontroller.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,12 @@
 class MessagesController < ApplicationController
   def index
     # render index page when going to /messages
+    id = Account.find_by(email: "jdoe@gmail.com")
+    @messages = Message.where(to_id: id)
+  end
+  def new
+  end
+  def sendMessage
+    redirect_to "/messages"
   end
 end
