13c7719d0b2ba8dfe2fd8bc7077f514cbc43e43e
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-21 19:01:07 -0600

    Implemented update method

--- app/controllers/student_checklists_controller.rb
+++ app/controllers/student_checklists_controller.rb
@@ -6,7 +6,22 @@ class StudentChecklistsController < ApplicationController
     @student_checklist = StudentChecklist.find_by(:student_id => student_id)
   end
 
-  def edit
+  def update
+    items = %w[citizenship_checkbox research_area_checkbox degree_objective_checkbox ug_inst_checkbox ug_gpa_checkbox
+               ug_degree_checkbox ug_major_checkbox ug_transcript_checkbox grad_inst_checkbox grad_gpa_checkbox
+               grad_degree_checkbox grad_major_checkbox grad_transcript_checkbox letter_recommendations_checkbox
+               language_scores_checkbox resume_checkbox sop_checkbox]
+    student_id = params[:id]
+    student_checklist = StudentChecklist.find(student_id)
+    items.each do |item|
+      if params.include? item
+        student_checklist.update("#{item.partition("_checkbox")[0]}": true)
+      else
+        student_checklist.update("#{item.partition("_checkbox")[0]}": false)
+      end
+    end
+    student_checklist.save!
 
+    redirect_to student_checklist_path
   end
 end
