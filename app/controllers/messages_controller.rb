3c08d8803173b240869ce010e0a6be595e3be3b6
abeschen <abeschen@uiowa.edu>
2022-11-22 16:56:51 -0600

    Updated sendMesage of messages_controller.rb to correctly send messages and fail.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,28 @@
 class MessagesController < ApplicationController
   def index
     # render index page when going to /messages
-    id = Account.find_by(email: "jdoe@gmail.com")
-    @messages = Message.where(to_id: id)
+    acc = Account.find_by(email: "jdoe@gmail.com")
+    @messages = Message.where(to_id: acc.id)
   end
   def new
   end
   def sendMessage
-    id = Account.find_by(email: "jdoe@gmail.com")
+    acc = Account.find_by(email: "jdoe@gmail.com")
+    to = params[:to]
+    to_acc = Account.find_by(email: to)
+    puts to_acc.nil?
+    if to_acc.nil?
+      flash[:warning] = "Please enter a correct email address"
+      redirect_to "/messages/new" and return
+    end
+    to_id = to_acc.id
+    subject = params[:subject]
+    body = params[:body]
+    if body.length == 0
+      flash[:warning] = "Please enter a body for your message"
+      redirect_to "/messages/new" and return
+    end
+    Message.create!(to_id: to_id, from_id: acc.id, to_email: to, from_email: acc.email, subject: subject, body: body)
     redirect_to "/messages"
   end
 end
