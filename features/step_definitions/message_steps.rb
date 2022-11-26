79a04b169a62202c6877ff7e462b5fe30ba2e18f
abeschen <abeschen@uiowa.edu>
2022-11-26 16:08:00 -0600

    Edited message_steps to add the message steps in.

--- features/step_definitions/message_steps.rb
+++ features/step_definitions/message_steps.rb
@@ -1,6 +1,12 @@
 Given('the following messages have been sent:') do |messages_table|
-  messages_table.each do |message|
+  messages_table.hashes.each do |message|
     Message.create!(message)
+  end
+end
+
+Given('the following accounts have been added:') do |accounts_table|
+  accounts_table.hashes.each do |account|
+    Account.create!(account)
   end
 end
 
@@ -8,26 +14,19 @@ When('I have visited the messages page') do
   visit '/messages'
 end
 
+When('I have visited the send messages page') do
+  visit '/messages/new'
+end
+
 When('I have logged in as {string}') do |string|
   pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I fill in the {string} with {string}') do |string, string2|
+  fill_in string, :with => string2
 end
 
 When('I have clicked the button {string}') do |string|
   click_button(string)
 end
 
-When('I have clicked {string}') do |string|
-  click_on string
-end
-
-When('I go to the url {string}') do |string|
-  visit string
-end
-
-Then('I should see {string}') do |string|
-  expect(page).to have_content(string)
-end
-
-Then('I should not see {string}') do |string|
-  expect(page).not_to have_content(string)
-end
