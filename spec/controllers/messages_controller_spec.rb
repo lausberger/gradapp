c31f843fb6c9019545aa0a91b756048ea0ee8bd5
abeschen <abeschen@uiowa.edu>
2022-11-27 12:16:43 -0600

    One more tests test.

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -17,11 +17,7 @@ describe MessagesController do
     context 'with a valid request' do
       it 'should redirect to my messages' do
         fake_results = double('account')
-        Account.create!(first_name: 'John',
-                        last_name: 'Doe',
-                        email: 'jdoe@gmail.com',
-                        password: '1234',
-                        type: 'Student')
+        create(:account)
         allow(Account).to receive(:find_by).with(email: 'jdoe@gmail.com').and_return fake_results
         allow(fake_results).to receive(:id).and_return '1'
         allow(fake_results).to receive(:email).and_return 'jdoe@gmail.com'
@@ -30,11 +26,7 @@ describe MessagesController do
       end
       it 'should say the message was sent' do
         fake_results = double('account1')
-        Account.create!(first_name: 'John',
-                        last_name: 'Doe',
-                        email: 'jdoe@gmail.com',
-                        password: '1234',
-                        type: 'Student')
+        create(:account)
         allow(Account).to receive(:find_by).with(email: 'jdoe@gmail.com').and_return fake_results
         allow(fake_results).to receive(:id).and_return '1'
         allow(fake_results).to receive(:email).and_return 'jdoe@gmail.com'
@@ -43,11 +35,7 @@ describe MessagesController do
       end
       it 'should return http response created' do
         fake_results = double('account1')
-        Account.create!(first_name: 'John',
-                        last_name: 'Doe',
-                        email: 'jdoe@gmail.com',
-                        password: '1234',
-                        type: 'Student')
+        create(:account)
         allow(Account).to receive(:find_by).with(email: 'jdoe@gmail.com').and_return fake_results
         allow(fake_results).to receive(:id).and_return '1'
         allow(fake_results).to receive(:email).and_return 'jdoe@gmail.com'
