122498adb40d6bd26f68ce2355ef08ad18aecc5f
abeschen <abeschen@uiowa.edu>
2022-11-27 00:54:18 -0600

    Testing spec tests with pipeline

--- spec/factories/account_factory.rb
+++ spec/factories/account_factory.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :account do
     first_name { 'John' }
     last_name { 'Doe' }
-    email { 'jdoe@gmail.com' }
+    email { 'test@gmail.com' }
     password { '1234' }
     type { 'Student' }
   end
