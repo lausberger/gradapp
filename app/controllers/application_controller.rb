a25dd8a6615c885a897cf6099956bd3d4fe50b15
Brandon Egger <brandonegger64@gmail.com>
2022-11-26 21:41:34 -0600

    added correct json file for testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
+require "google/cloud/storage"
 
 class ApplicationController < ActionController::Base
+  @@student_document_bucket
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :load_gcloud
+
+  private
+
+  def load_gcloud
+    storage = Google::Cloud::Storage.new(
+      project_id: "selt-grad-app",
+      credentials: "selt-grad-app-287ce054d81a.json"
+    )
+    @@student_document_bucket = storage.bucket 'selt-grad-app-student-documents'
+  end
+
 end
