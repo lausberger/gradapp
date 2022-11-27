16fddbd28e3a4f9b606d25dd85f191e622a63214
abeschen <abeschen@uiowa.edu>
2022-11-27 15:21:02 -0600

    Misplaced inverse

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Faculty < ActiveRecord::Base
-  belongs_to :account
+  belongs_to :account, inverse_of: :faculty
 
-  validates :topic_area, presence: true, allow_blank: false, inverse_of: :faculty
+  validates :topic_area, presence: true, allow_blank: false
 end
