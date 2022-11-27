6376c5344feddcf89fba095236b36a200e897650
abeschen <abeschen@uiowa.edu>
2022-11-27 15:31:36 -0600

    Fixed rubocop testing for inverses

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Message < ActiveRecord::Base
-  belongs_to :to, class_name: 'Account', inverse_of: :account
-  belongs_to :from, class_name: 'Account', inverse_of: :account
+  belongs_to :to, class_name: 'Account'
+  belongs_to :from, class_name: 'Account'
   validates :to_id, :from_id, :to_email, :from_email, :body, presence: true
 end
