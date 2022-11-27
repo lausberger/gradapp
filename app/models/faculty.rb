aab0e5c598d6360b7d33bc08945bb6fc13b64000
abeschen <abeschen@uiowa.edu>
2022-11-27 15:25:19 -0600

    Another inverse test

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Faculty < ActiveRecord::Base
-  belongs_to :account
+  belongs_to :account, inverse_of: :account
 
   validates :topic_area, presence: true, allow_blank: false
 end
