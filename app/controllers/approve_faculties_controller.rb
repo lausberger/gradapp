c1439b6c0c037e88f7f870592fedcea888a4bf65
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-28 15:12:01 -0600

    Added controller and view for approving faculty members

---
+++ app/controllers/approve_faculties_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Controller to Approve to Faculty Account
+class ApproveFacultiesController < ApplicationController
+  def show
+
+  end
+
+  def update
+
+  end
+end
