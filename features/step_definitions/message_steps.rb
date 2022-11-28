26c1c9e3276c95751ad9b049380ba38c5b9b9687
abeschen <abeschen@uiowa.edu>
2022-11-27 23:05:44 -0600

    Updated Steps to work with ids so messages could be seen.

--- features/step_definitions/message_steps.rb
+++ features/step_definitions/message_steps.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+Given('the following messages have been sent to John Doe:') do |messages_table|
+  acc = Account.find_by(email: 'jdoe@gmail.com')
+  messages_table.hashes.each do |message|
+    message[:to_id] = acc.id
+    Message.create!(message)
+  end
+end
+
 Given('the following messages have been sent:') do |messages_table|
   messages_table.hashes.each do |message|
     Message.create!(message)
@@ -21,7 +29,6 @@ When('I have visited the send messages page') do
   visit '/messages/new'
 end
 
-
 When('I fill in the {string} with {string}') do |string, string2|
   fill_in string, with: string2
 end
@@ -32,8 +39,8 @@ end
 
 When('I have logged in as John Doe') do
   visit login_path
-  fill_in 'Email', with: "jdoe@gmail.com"
-  fill_in 'Password', with: "1234"
+  fill_in 'Email', with: 'jdoe@gmail.com'
+  fill_in 'Password', with: '1234'
   click_button 'Log in'
 end
 
