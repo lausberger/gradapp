b0d44a644f164b6ae8eb45d7da9fa0a0e66de352
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-29 09:29:45 -0600

    Added login and account_type check

--- app/controllers/approve_faculties_controller.rb
+++ app/controllers/approve_faculties_controller.rb
@@ -2,7 +2,13 @@
 
 # Controller to Approve to Faculty Account
 class ApproveFacultiesController < ApplicationController
+  before_action :require_login
   def index
+    # puts @current_user.id
+    #puts current_user.account_type
+    if @current_user.account_type != 'Department Chair'
+      flash[:alert] = 'You must be a department chair to approve new accounts'
+    end
     @approval_needed_accounts = Faculty.where(approved: false)
   end
 
