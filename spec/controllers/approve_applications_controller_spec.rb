ed2379d0e8648da19e8c703b3de1945ee677728b
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-11-30 17:44:51 -0600

    Created empty RSpec controller tests for approve applications

---
+++ spec/controllers/approve_applications_controller_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rails_helper'
+
+if RUBY_VERSION >= '2.6.0'
+  if Rails.version < '5'
+    module ActionController
+      class TestResponse < ActionDispatch::TestResponse
+        def recycle!
+          # HACK: to avoid MonitorMixin double-initialize error:
+          @mon_mutex_owner_object_id = nil
+          @mon_mutex = nil
+          initialize
+        end
+      end
+    end
+  else
+    puts 'Monkeypatch for ActionController::TestResponse no longer needed'
+  end
+end
+
+describe ApproveApplicationsController do
+end
