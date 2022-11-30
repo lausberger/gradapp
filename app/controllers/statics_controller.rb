2f2a96535454dd8e09f22f20cb80c5e5789c87fa
Evan Lemker <lemkere@gmail.com>
2022-11-30 01:12:35 -0600

    Corrected implementation to conform to feature test

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -3,17 +3,17 @@
 # rubocop: disable Rails/ApplicationController
 
 # Main controller for handling static page generation
-class StaticsController < ActionController::Base
+class StaticsController < ApplicationController
   helper_method :current_user, :home
 
   def home
-    if not !!@current_user then
+    if not !!current_user then
       render 'public_home'
-    elsif @current_user.account_type == "Student" then
+    elsif current_user.account_type == "Student" then
       render 'student_home'
-    elsif @current_user.account_type == "Faculty" then
+    elsif current_user.account_type == "Faculty" then
       render 'faculty_home'
-    elsif @current_user.account_type == "Chair" then
+    elsif current_user.account_type == "Chair" then
       render 'chair_home'
     else
       render 'public_home'
