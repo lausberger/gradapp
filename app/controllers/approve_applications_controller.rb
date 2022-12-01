e587426fe0f55937d0a6b155d2a2bade24d9d900
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-12-01 08:27:35 -0600

    feat: implemented approve application controller

--- app/controllers/approve_applications_controller.rb
+++ app/controllers/approve_applications_controller.rb
@@ -2,4 +2,20 @@
 
 # Controller to Approve to Faculty Account
 class ApproveApplicationsController < ApplicationController
+  before_action :require_login
+
+  def index
+    acc = Account.find_by(id: @current_user)
+    if acc.account_type != 'Department Chair'
+      flash[:alert] = 'You must be a department chair to make decisions on graduate applications'
+    end
+    @application = GraduateApplication.where(status: 'submitted')
+  end
+
+  def update
+    approved_application = GraduateApplication.find(params[:approve])
+    denied_application = GraduateApplication.find(params[:deny])
+    approved_application&.update(status: 'approved')
+    denied_application&.update(status: 'denied')
+  end
 end
