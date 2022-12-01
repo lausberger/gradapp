98650475c3e87c8bcd338733ac1c62d17828161b
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-12-01 08:21:05 -0600

    feat: added RSpec tests for approve applications

--- spec/controllers/approve_applications_controller_spec.rb
+++ spec/controllers/approve_applications_controller_spec.rb
@@ -21,4 +21,53 @@ if RUBY_VERSION >= '2.6.0'
 end
 
 describe ApproveApplicationsController do
+  describe 'view application approval page' do
+    before(:each) do
+      application = {
+        first_name: 'Jack',
+        last_name: 'Stockley',
+        email: 'jack-stockley@uiowa.edu',
+        phone: '8722214561',
+        dob: Date.new,
+        status: 'submitted'
+      }
+      @grad_application = GraduateApplication.create!(application)
+    end
+    it 'should redirect to login page' do
+      get :index
+      expect(response).to redirect_to login_path
+    end
+    it 'should flash message saying you aren\'t signed in as dept. chair' do
+      acc = create(:account, :faculty)
+      allow(controller).to receive(:logged_in?).and_return(true)
+      allow(Account).to receive(:find_by).and_return acc
+      get :index
+      expect(response).to render_template('index')
+      expect(flash[:alert]).to be_present
+    end
+    it 'should show applications needing approval' do
+      acc = create(:account, :department_chair)
+      allow(controller).to receive(:logged_in?).and_return(true)
+      allow(Account).to receive(:find_by).and_return acc
+      get :index
+      expect(response).to render_template('index')
+      expect(flash[:alert]).to_not be_present
+    end
+    describe 'make decision on application' do
+      it 'should approve grad application and render show template' do
+        acc = create(:account, :department_chair)
+        allow(controller).to receive(:logged_in?).and_return(true)
+        allow(Account).to receive(:find_by).and_return acc
+        put :update, { approve: @grad_application.id }
+        expect(response).to redirect_to approve_faculties_path
+      end
+      it 'should deny grad application and render show template' do
+        acc = create(:account, :department_chair)
+        allow(controller).to receive(:logged_in?).and_return(true)
+        allow(Account).to receive(:find_by).and_return acc
+        put :update, { deny: @grad_application.id }
+        expect(response).to redirect_to approve_faculties_path
+      end
+    end
+  end
 end
