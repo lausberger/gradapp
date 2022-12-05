9dc5c4a96e05c5c01634bbd2296b8e284180d4be
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-12-05 11:42:08 -0600

    fix: updated seeds to work with new discussion table schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,53 +8,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-discussions = [
-  { title: 'What\'s the best Graduate Program?',
-    body: 'Hey guys, I\'m having a hard time picking a graduate program.',
-    author: 'Jack Stockley',
-    root_discussion_id: -1 },
-  { title: 'Contact Faculty',
-    body: 'Hello, what\'s the best way to contact a faculty member?',
-    author: 'Joe Smith',
-    root_discussion_id: -1 },
-  { title: 'Submit Application',
-    body: 'How do I submit an application here?',
-    author: 'Hans Johnson',
-    root_discussion_id: -1 },
-  { title: '',
-    body: 'I heard the CSE program is great',
-    author: 'Hans Johnson',
-    root_discussion_id: 1 },
-  { title: '',
-    body: 'Email',
-    author: 'Jack Stockley',
-    root_discussion_id: 2 },
-  { title: '',
-    body: 'Yea, CSE is great at Iowa!',
-    author: 'Alberto Segre',
-    root_discussion_id: 1 },
-  { title: '',
-    body: 'In the future, you can do it right here!',
-    author: 'Jack Stockley',
-    root_discussion_id: 2 },
-  { title: '',
-    body: 'Right now you can\'t, it\'s a work in progress!',
-    author: 'Jack Stockley',
-    root_discussion_id: 3 },
-  { title: '',
-    body: 'Thanks, Jack',
-    author: 'Hans Johnson',
-    root_discussion_id: 3 },
-  { title: '',
-    body: 'You can\'t go wrong with any of their programs',
-    author: 'Julie Maxwell',
-    root_discussion_id: 1 }
-]
-
-discussions.each do |discussion|
-  Discussion.create!(discussion)
-end
-
 applications = [
   {
     first_name: 'Brandon',
@@ -136,6 +89,53 @@ accounts = [
 
 accounts.each do |account|
   Account.create! account
+end
+
+discussions = [
+  { title: 'What\'s the best Graduate Program?',
+    body: 'Hey guys, I\'m having a hard time picking a graduate program.',
+    author_id: Account.find_by(first_name: 'Jack').id,
+    root_discussion_id: -1 },
+  { title: 'Contact Faculty',
+    body: 'Hello, what\'s the best way to contact a faculty member?',
+    author_id: Account.find_by(first_name: 'Alex').id,
+    root_discussion_id: -1 },
+  { title: 'Submit Application',
+    body: 'How do I submit an application here?',
+    author_id: Account.find_by(first_name: 'Hans').id,
+    root_discussion_id: -1 },
+  { title: '',
+    body: 'I heard the CSE program is great',
+    author_id: Account.find_by(first_name: 'Hans').id,
+    root_discussion_id: 1 },
+  { title: '',
+    body: 'Email',
+    author_id: Account.find_by(first_name: 'Jack').id,
+    root_discussion_id: 2 },
+  { title: '',
+    body: 'Yea, CSE is great at Iowa!',
+    author_id: Account.find_by(first_name: 'Jonah').id,
+    root_discussion_id: 1 },
+  { title: '',
+    body: 'In the future, you can do it right here!',
+    author_id: Account.find_by(first_name: 'Jack').id,
+    root_discussion_id: 2 },
+  { title: '',
+    body: 'Right now you can\'t, it\'s a work in progress!',
+    author_id: Account.find_by(first_name: 'Jack').id,
+    root_discussion_id: 3 },
+  { title: '',
+    body: 'Thanks, Jack',
+    author_id: Account.find_by(first_name: 'Hans').id,
+    root_discussion_id: 3 },
+  { title: '',
+    body: 'You can\'t go wrong with any of their programs',
+    author_id: Account.find_by(first_name: 'Alex').id,
+    root_discussion_id: 1 }
+]
+
+discussions.each do |discussion|
+  Discussion.create!(discussion)
 end
 
 stud_checklists = [
