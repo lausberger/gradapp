52a8351d681b7c481ac37f690fab60660ac9fc5c
Lucas Ausberger <ausbergerl@hotmail.com>
2022-12-05 18:14:50 +0000

    fix: fixed merge conflicts with main

---
+++ app/controllers/application_decisions_controller.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+# Controller to Approve to Faculty Account
+class ApplicationDecisionsController < ApplicationController
+  before_action :require_login
+
+  def index
+    acc = Account.find_by(id: @current_user)
+    if acc.account_type != 'Department Chair'
+      flash[:alert] = 'You must be a department chair to make decisions on graduate applications'
+    end
+    @applications = GraduateApplication.where(status: 'submitted')
+  end
+
+  def update
+    status = params[:commit].downcase
+    application_id = params[:id]
+    application = GraduateApplication.find(application_id)
+    case status
+    when 'approve'
+      application.update(status: 'accepted')
+    when 'deny'
+      application.update(status: 'denied')
+    else
+      application.update(status: 'submitted')
+    end
+    redirect_to application_decisions_path
+  end
+end
