38282d2aa8bce6357578b00f4510169839a58d7e
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-12-05 15:29:42 -0600

    fix: Updated new and delete, discussion controller RSpec tests to work with new discussions setup

--- spec/controllers/discussion_controller_spec.rb
+++ spec/controllers/discussion_controller_spec.rb
@@ -116,5 +116,143 @@ describe DiscussionsController do
       expect(response).to redirect_to discussion_path(@discussion_reply[:root_discussion_id])
     end
   end
-
+  describe 'editing discussions' do
+    before(:each) do
+      account = {
+        first_name: 'Jack',
+        last_name: 'Stockley',
+        email: 'jack-stockley@uiowa.edu',
+        password: 'Password123',
+        password_confirmation: 'Password123',
+        account_type: 'Student'
+      }
+      Account.create! account
+      @discussion = {
+        title: 'Test',
+        body: 'Test',
+        account_id: Account.find_by(first_name: 'Jack').id,
+        root_discussion_id: -1
+      }
+      Discussion.create! @discussion
+    end
+    it 'should redirect to login page' do
+      put :update, { id: Discussion.find_by(title: 'Test').id, discussion: @discussion }
+      expect(response).to redirect_to login_path
+    end
+    it 'should show all discussions' do
+      acc = create(:account)
+      allow(controller).to receive(:logged_in?).and_return(true)
+      allow(Account).to receive(:find_by).and_return acc
+      put :update, { id: Discussion.find_by(title: 'Test').id, discussion: @discussion }
+      expect(response).to redirect_to discussions_path
+    end
+  end
+  describe 'editing discussions replies' do
+    before(:each) do
+      account = {
+        first_name: 'Jack',
+        last_name: 'Stockley',
+        email: 'jack-stockley@uiowa.edu',
+        password: 'Password123',
+        password_confirmation: 'Password123',
+        account_type: 'Student'
+      }
+      Account.create! account
+      discussion = {
+        title: 'Test',
+        body: 'Test',
+        account_id: Account.find_by(first_name: 'Jack').id,
+        root_discussion_id: -1
+      }
+      Discussion.create! discussion
+      @discussion_reply = {
+        title: 'Hello',
+        body: 'Hello World',
+        account_id: Account.find_by(first_name: 'Jack').id,
+        root_discussion_id: Discussion.find_by(title: 'Test').id
+      }
+      Discussion.create! @discussion_reply
+    end
+    it 'should redirect to login page' do
+      put :update, { id: Discussion.find_by(title: 'Hello').id, discussion: @discussion_reply }
+      expect(response).to redirect_to login_path
+    end
+    it 'should show all discussion replies' do
+      acc = create(:account)
+      allow(controller).to receive(:logged_in?).and_return(true)
+      allow(Account).to receive(:find_by).and_return acc
+      put :update, { id: Discussion.find_by(title: 'Hello').id, discussion: @discussion_reply }
+      expect(response).to redirect_to discussion_path(Discussion.find_by(title: 'Hello')[:root_discussion_id])
+    end
+  end
+  describe 'deleting discussions' do
+    before(:each) do
+      account = {
+        first_name: 'Jack',
+        last_name: 'Stockley',
+        email: 'jack-stockley@uiowa.edu',
+        password: 'Password123',
+        password_confirmation: 'Password123',
+        account_type: 'Student'
+      }
+      Account.create! account
+      @discussion = {
+        title: 'Test',
+        body: 'Test',
+        account_id: Account.find_by(first_name: 'Jack').id,
+        root_discussion_id: -1
+      }
+      Discussion.create! @discussion
+    end
+    it 'should redirect to login page' do
+      delete :destroy, { id: Discussion.find_by(title: 'Test').id }
+      expect(response).to redirect_to login_path
+    end
+    it 'should show all discussions' do
+      acc = create(:account)
+      allow(controller).to receive(:logged_in?).and_return(true)
+      allow(Account).to receive(:find_by).and_return acc
+      delete :destroy, { id: Discussion.find_by(title: 'Test').id }
+      expect(response).to redirect_to discussions_path
+    end
+  end
+  describe 'deleting discussion replies' do
+    before(:each) do
+      account = {
+        first_name: 'Jack',
+        last_name: 'Stockley',
+        email: 'jack-stockley@uiowa.edu',
+        password: 'Password123',
+        password_confirmation: 'Password123',
+        account_type: 'Student'
+      }
+      Account.create! account
+      discussion = {
+        title: 'Test',
+        body: 'Test',
+        account_id: Account.find_by(first_name: 'Jack').id,
+        root_discussion_id: -1
+      }
+      Discussion.create! discussion
+      @discussion_reply = {
+        title: 'Hello',
+        body: 'Hello World',
+        account_id: Account.find_by(first_name: 'Jack').id,
+        root_discussion_id: Discussion.find_by(title: 'Test').id
+      }
+      Discussion.create! @discussion_reply
+    end
+    it 'should redirect to login page' do
+      delete :destroy, { id: Discussion.find_by(title: 'Hello').id }
+      expect(response).to redirect_to login_path
+    end
+    it ' should show all discussion replies' do
+      acc = create(:account)
+      allow(controller).to receive(:logged_in?).and_return(true)
+      allow(Account).to receive(:find_by).and_return acc
+      root_discussion_id = Discussion.find_by(title: 'Hello')[:root_discussion_id]
+      delete :destroy, { id: Discussion.find_by(title: 'Hello').id }
+      expect(response).to redirect_to discussion_path(root_discussion_id)
+    end
+  end
 end
