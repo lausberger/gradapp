3315fa4b0f278da7caa16103d7ce55437359e062
Jack Stockley <34308937+jnstockley@users.noreply.github.com>
2022-12-07 08:09:15 -0600

    fix: Updated migrations to work with newly merged feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 30721148119015) do
+ActiveRecord::Schema.define(version: 30721148119016) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "first_name",      null: false
