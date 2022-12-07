92003a78e17b2e6f35ad594730ef82607fc615d5
Evan Lemker <lemkere@gmail.com>
2022-12-07 15:17:48 -0600

    feat: migrated db changes with research area addition

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 30721148119016) do
+ActiveRecord::Schema.define(version: 30721148119017) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "first_name",      null: false
@@ -90,6 +90,13 @@ ActiveRecord::Schema.define(version: 30721148119016) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "messages_id"
+  end
+
+  create_table "research_areas", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "overview",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "student_checklists", force: :cascade do |t|
