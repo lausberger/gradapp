1300b923568e90930339e36573107014e319f36f
Evan Lemker <lemkere@gmail.com>
2022-12-07 16:28:34 -0600

    fix: Corrected seeds with research_area

--- db/seeds.rb
+++ db/seeds.rb
@@ -162,46 +162,30 @@ stud_checklists.each do |checklist|
   StudentChecklist.create! checklist
 end
 
-faculties = [
-  {
-    account_id: Account.find_by(first_name: 'Jack').id,
-    topic_area: 'Applied Physics',
-    approved: true
-  },
-  {
-    account_id: Account.find_by(first_name: 'Hans').id,
-    topic_area: 'Bioinformatics',
-    approved: true
-  },
-  {
-    account_id: Account.find_by(first_name: 'Alex').id,
-    topic_area: 'Integrated Circuits',
-    approved: false
-  }
-]
-
-faculties.each do |faculty|
-  Faculty.create! faculty
-end
-
 research_areas = [
   {
     title: 'Applied Physics',
-    overview: "Students majoring in ECE with an Applied Physics emphasis develop the devices, systems, and technologies
+    summary: 'The application of physics to solve scientific or engineering problems. It is usually considered to be a
+              bridge or a connection between physics and engineering.',
+    detailed_overview: "Students majoring in ECE with an Applied Physics emphasis develop the devices, systems, and technologies
                used to implement modern electronics devices and systems. Further, they support research into new
                technologies that, for instance, could extend Moore's Law or allow for development of quantum scale
                devices beyond Moore's law."
   },
   {
     title: 'Bioinformatics',
-    overview: "Bioinformatics is an emerging discipline that uses mathematics and computer engineering to help answer
+    summary: 'The application of tools of computation and analysis to the capture and interpretation of biological data.
+              It is an interdisciplinary field, which harnesses computer science, mathematics, physics, and biology',
+    detailed_overview: "Bioinformatics is an emerging discipline that uses mathematics and computer engineering to help answer
                questions in bioscience, agriculture and medicine. Bioinformatics harnesses the power of recent
                developments in statistics, software engineering, high-performance computing, and artificial intelligence
                to sort through and prioritize vast datasets to find clues to the causes and cures of human disease."
   },
   {
     title: 'Computer Networks',
-    overview: "Network engineers design, develop and configure, and maintain the high-speed wired and wireless digital
+    summary: 'Interconnected computing devices that can exchange data and share resources with each other. These networked
+              devices use a system of rules, called communications protocols, to transmit information over physical or wireless technologies.',
+    detailed_overview: "Network engineers design, develop and configure, and maintain the high-speed wired and wireless digital
                networks that carry vast amounts of data, enabling the Internet, cell phones, television, and every other
                kind of electronic information exchange. This EFA combines a background in computer hardware and software
                (gained through the computer track of the curriculum) with additional knowledge of computer networks,
@@ -209,14 +193,17 @@ research_areas = [
   },
   {
     title: 'Integrated Circuits',
-    overview: 'Integrated circuit engineers develop and design circuits that play a central role in countless modern
+    summary: 'A semiconductor wafer on which thousands or millions of tiny resistors, capacitors, diodes and transistors are fabricated.',
+    detailed_overview: 'Integrated circuit engineers develop and design circuits that play a central role in countless modern
                electronic devices, appliances, and vehicles. These circuits add smarts and complex functionality to
                wide variety of devices including computers, phones, medical instruments and implants, automobiles,
                 power generation and distribution systems, airplanes, consumer electronic devices, and home appliances.'
   },
   {
     title: 'Signal and Image Processing',
-    overview: 'Depending on the courses selected, completion of signal and image processing area coursework can provide
+    summary: 'The field of signal and image processing encompasses the theory and practice of algorithms and hardware that
+              convert signals produced by artificial or natural means into a form useful for a specific purpose.',
+    detailed_overview: 'Depending on the courses selected, completion of signal and image processing area coursework can provide
                additional understanding of topics in signal sampling and compression, detection and estimation, signal
                feature extraction, time-frequency analysis, speech, audio, image and video processing, machine learning
                and data mining, biomedical signals, sensor networks, and the signal processing aspects of communication
@@ -227,3 +214,27 @@ research_areas = [
 research_areas.each do |research_area|
   ResearchArea.create! research_area
 end
+
+
+faculties = [
+  {
+    account_id: Account.find_by(first_name: 'Jack').id,
+    research_area_id: ResearchArea.find_by(title: 'Applied Physics').id,
+    approved: true
+  },
+  {
+    account_id: Account.find_by(first_name: 'Hans').id,
+    research_area_id: ResearchArea.find_by(title: 'Bioinformatics').id,
+    approved: true
+  },
+  {
+    account_id: Account.find_by(first_name: 'Alex').id,
+    research_area_id: ResearchArea.find_by(title: 'Integrated Circuits').id,
+    approved: false
+  }
+]
+
+faculties.each do |faculty|
+  Faculty.create! faculty
+end
+
