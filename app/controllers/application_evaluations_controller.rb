8c3f0151ac3f7e8644cc8221724e059eb04a0f1b
Brandon Egger <beggr@uiowa.edu>
2022-12-09 22:24:36 -0600

    feat: added application exists validation logic

--- app/controllers/application_evaluations_controller.rb
+++ app/controllers/application_evaluations_controller.rb
@@ -3,16 +3,33 @@
 # Controller for creating application evaluations for an application
 class ApplicationEvaluationsController < ApplicationController
   before_action :require_login
-
-  def new
-    # TODO: implement
-  end
+  before_action :check_if_application_exists, only: %i[create update]
 
   def create
-    # TODO: implement
+    Rails.logger.debug params.to_hash
   end
 
   def update
-    # TODO:
+    # TODO: complete update route
+  end
+
+  private
+
+  def check_if_application_exists
+    unless params.key?('application_evaluation') && params[:application_evaluation].key?('graduate_application_id')
+      flash[:notice] = 'Unable to find the application with that ID'
+      redirect_to graduate_application_path
+      return
+    end
+
+    @graduate_application = GraduateApplication.find(params[:application_evaluation][:graduate_application_id])
+    return unless @graduate_application.nil?
+
+    flash[:notice] = 'Unable to find the application with that ID'
+    redirect_to graduate_application_path
+  end
+
+  def graduate_application_params
+    params.require(:application_evaluation).permit(:score, :comment, :graduate_application_id)
   end
 end
