f89de36b292093d5f90156724c59b0afccc149c5
Brandon Egger <beggr@uiowa.edu>
2022-12-09 21:22:21 -0600

    feat: add basic structer to evaluations controller

--- spec/controllers/application_evaluations_controller_spec.rb
+++ spec/controllers/application_evaluations_controller_spec.rb
@@ -4,13 +4,5 @@ require 'spec_helper'
 require 'rails_helper'
 
 describe ApplicationEvaluationsController do
-  before_action :require_login
 
-  def new
-    # TODO: implement
-  end
-
-  def create
-    # TODO: implement
-  end
 end
