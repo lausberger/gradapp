96731a51e9373f76123c2242f41994d5e95c692e
Brandon Egger <beggr@uiowa.edu>
2022-12-09 19:39:12 -0600

    test: added base rspec files for evaluations model/controller

---
+++ spec/controllers/application_evaluations_controller_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rails_helper'
+
+describe ApplicationEvaluationsController do
+  before_action :require_login
+
+  def new
+    #TODO:
+  end
+
+  def create
+    #TODO:
+  end
+end
---
+++ spec/models/application_evaluation_spec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rails_helper'
+
+describe ApplicationEvaluation do
+  # TODO:
+end
