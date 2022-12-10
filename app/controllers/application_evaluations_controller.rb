d37c525b72e9bc6b58b7944fbd5ffc7b2f4bfa9b
Brandon Egger <beggr@uiowa.edu>
2022-12-09 22:47:27 -0600

    feat: added create route functionality to application evaluations controller

--- app/controllers/application_evaluations_controller.rb
+++ app/controllers/application_evaluations_controller.rb
@@ -3,10 +3,18 @@
 # Controller for creating application evaluations for an application
 class ApplicationEvaluationsController < ApplicationController
   before_action :require_login
-  before_action :check_if_application_exists, only: %i[create update]
+  before_action :check_if_application_exists, :format_params, only: %i[create update]
 
   def create
-    Rails.logger.debug params.to_hash
+    unless params.key?('application_evaluation')
+      flash[:warning] = 'Application evaluation creation failed'
+      redirect_to graduate_application_path(@graduate_application)
+    end
+
+    @graduate_application.application_evaluations.create(params[:application_evaluation].to_hash)
+    flash[:warning] = 'Application evaluation creation failed' unless @graduate_application.valid?
+
+    redirect_to graduate_application_path(@graduate_application)
   end
 
   def update
@@ -15,21 +23,29 @@ class ApplicationEvaluationsController < ApplicationController
 
   private
 
+  def format_params
+    return unless params.key?('application_evaluation') && params[:application_evaluation].key?('score')
+    return unless params[:application_evaluation][:score].respond_to? :to_i
+
+    params[:application_evaluation][:score] = params[:application_evaluation][:score].to_i
+  end
+
   def check_if_application_exists
     unless params.key?('application_evaluation') && params[:application_evaluation].key?('graduate_application_id')
-      flash[:notice] = 'Unable to find the application with that ID'
-      redirect_to graduate_application_path
+      flash[:warning] = 'Unable to find the application with that ID'
+      redirect_to graduate_applications_path
       return
     end
 
     @graduate_application = GraduateApplication.find(params[:application_evaluation][:graduate_application_id])
+    params[:application_evaluation].delete(:graduate_application_id)
     return unless @graduate_application.nil?
 
-    flash[:notice] = 'Unable to find the application with that ID'
-    redirect_to graduate_application_path
+    flash[:warning] = 'Unable to find the application with that ID'
+    redirect_to graduate_applications_path
   end
 
-  def graduate_application_params
+  def application_evaluation_params
     params.require(:application_evaluation).permit(:score, :comment, :graduate_application_id)
   end
 end
